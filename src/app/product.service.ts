62e950b27c1e4a9565154de7e1f3a6e0c830da47
Hardik Sodhani <HSodhani@ATD-US.com>
2019-02-03 22:20:05 -0600

    fixing issues

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,9 +13,8 @@ export class ProductService {
   }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => {
-      return response.json();
-    })
+    return this._http.get(this._albumUrl).map((response) =>
+      response.json());
   }
 
 }
